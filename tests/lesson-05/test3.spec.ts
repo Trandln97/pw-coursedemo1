7937a4dc2b4d3e688b38e33b212ff675ddfc6ea5
test 01 - Tran Dang <Trandangln@gmail.com>
2025-11-07 22:49:28 +0700

    chore: fix convention file test3

--- tests/lesson-05/test3.spec.ts
+++ tests/lesson-05/test3.spec.ts
@@ -7,8 +7,8 @@ test('Ex3.test', async ({ page }) => {
   await page.getByText('Bài học 3: Todo page').click();
 
   // Tạo 100 task
-  const input = page.locator('#new-task');
-  const addBtn = page.locator('button', { hasText: /add/i });
+  const input  = page.locator('//input[@id="new-task"]');
+  const addBtn = page.locator('//button[normalize-space()="Add Task"]');
 
   for (let i = 1; i <= 100; i++) {
     await input.fill(`Todo ${i}`);
